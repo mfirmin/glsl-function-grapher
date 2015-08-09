2470d7f8821dd989006a3c20b07186e9b23e37cd
Michael Firmin <mcfirmin@gmail.com>
2015-08-09 13:42:12 -0700

    asdf

--- templates/static/bundle.js
+++ templates/static/bundle.js
@@ -9832,7 +9832,6 @@ world.addEntity(box);
 
 function updateShader(fn) {
     var fragShader = makeFragmentShader(fn);
-    console.log(fragShader);
     box.opts.material.fragmentShader = fragShader;
     box.opts.material.needsUpdate = true;
 }
