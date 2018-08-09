2fff15b5bb75dd3749464b84989896d4b0409443
Michael Firmin <mcfirmin@gmail.com>
2018-08-09 14:31:06 -0400

    Fix bugs with variables and coeffs inside powers, and add Sphere example

--- src/components/eqn-edit.js
+++ src/components/eqn-edit.js
@@ -24,7 +24,7 @@ Vue.component('eqn-edit', {
                 <span class="variable" v-else-if="node.type === 'variable'">{{ node.id }}</span>
                 <span v-else-if="node.type === 'static'">{{ node.value }}</span>
                 <span v-else-if="node.type === 'power'">
-                    <eqn-edit :equationhtml="node.value"></eqn-edit><sup>{{ node.power }}</sup>
+                    <eqn-edit :equationhtml="node.value" @coefficient-changed="$emit('coefficient-changed', $event)"></eqn-edit><sup>{{ node.power }}</sup>
                 </span>
                 <span v-else-if="node.type === 'error'" class="error">{{ node.value }}</span>
             </span>
